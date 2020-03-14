766d069309d2cbe6dd46ed3931f62d3fac32e62e
Edoardo <edoardo.costantini.96@gmail.com>
2020-03-14 15:00:16 +0100

    "" should return zero, passed.

--- strings/Strings/StringCalculator.cs
+++ strings/Strings/StringCalculator.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Strings
 {
     public class StringCalculator
     {
-        
+        public int Add(string numbers)
+        {
+            int result=0;
+
+            if (numbers.Length == 0)
+                return result;
+            else
+                return result;
+        }
     }
 }
